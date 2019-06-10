b5e4433be2669992f5d0c731bf8c46a2e1b4ae6f
Artyom Bondartsov <erithion@gmail.com>
2019-06-10 21:24:30 +0300

    fenwick tree

---
+++ algo_cpp/tree_fenwick.cmake
@@ -0,0 +1,26 @@
+cmake_minimum_required      ( VERSION 3.12.0 )
+
+set                         ( PROJECT_NAME                       fenwick_tree )
+set                         ( INSTALLDIR                         ${BUILDDIR}/cpp/ )
+set                         ( SRC_FILES                          tree_fenwick.cpp )
+set                         ( INC_FILES                          )
+
+# compiler/linker flags
+string                      ( APPEND CMAKE_CXX_FLAGS             " /EHsc" )
+string                      ( APPEND CMAKE_CXX_FLAGS             " /std:c++14" )
+string                      ( APPEND CMAKE_SHARED_LINKER_FLAGS   " /SAFESEH:NO" )
+message                     ( STATUS                             "CXX flags ${CMAKE_CXX_FLAGS}" )
+message                     ( STATUS                             "Linker flags ${CMAKE_SHARED_LINKER_FLAGS}" )
+         
+project                     ( ${PROJECT_NAME} )
+
+include_directories         ( ${CMAKE_CURRENT_SOURCE_DIR} )
+     
+
+add_executable              ( ${PROJECT_NAME}                    ${SRC_FILES} 
+                                                                 ${INC_FILES} )
+
+target_link_libraries       ( ${PROJECT_NAME}                     )
+
+install                     ( TARGETS                            ${PROJECT_NAME}                         
+                              DESTINATION                        ${INSTALLDIR} )
